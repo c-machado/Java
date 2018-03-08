817ea9f09ac495b557719d6822523e60ce6775ce
Carolina Machado <machadoca@google.com>
2018-03-08 17:10:56 -0500

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","drivers//chromedriver");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.netflix.com/co/login");
+        //browser.findElement(By.className("ui-text-input")).sendKeys("carolina");
+        //browser.findElement(By.cssSelector(".login-input-email input")).sendKeys("dcmachadou@gmail.com");
+        browser.findElement(By.cssSelector(".login-input-email input:first-of-type")).sendKeys("dcmachadou@gmail.com");
+
+        browser.findElement(By.cssSelector("input[type='password']")).sendKeys("gabriel1031");
+        browser.findElement(By.cssSelector(".login-button")).click();
+
+        //browser.quit();
+        //System.out.println(browser.getTitle());
+        //Methods obj=new Methods();
+        //System.out.println(obj.addTwoNumbers(3,4));
+    }
+}
---
+++ src/Methods.java
@@ -0,0 +1,8 @@
+public class Methods {
+
+    public int addTwoNumbers(int a, int b){
+        return a + b;
+    }
+
+
+}
